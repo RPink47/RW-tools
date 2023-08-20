13388b00424c410074c90a78a5161c03bfd7599e
RPink47 <robinorichter47@gmail.com>
2023-08-20 17:25:41 +0200

    Aliases now fully work!!!!

--- DevTools.js
+++ DevTools.js
@@ -7,6 +7,22 @@ function makeItemList() {
     });
     out = (uniq(out))
     out = out.map(e => e.split(" ").map(e => e.charAt(0).toUpperCase() + e.slice(1)))
-    out = out.map(e => { return { name: e.join(" "), wiki: "https://rainworld.miraheze.org/wiki/".concat(e.join("_")) } })
-    console.log(JSON.stringify(out));
+    out = out.map(e => {
+        return { name: e.join(" "), wiki: "https://rainworld.miraheze.org/wiki/" + (e.join("_")) }
+    })
+    console.log(out.map(e => JSON.stringify(e)).join(",\n"));
+}
+function JSONtoCrafts(input) {
+    out = input.map(e => { return { a: e[0], b: e[1], out: e[2] } });
+    out = out.map()
+    console.log(out.map(e => JSON.stringify(e)).join(",\n"));
+}
+
+function httpGet(theUrl) {
+    var xmlHttp = null;
+
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", theUrl, false);
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
 }
